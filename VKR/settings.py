65f33c651e68cd40ac1b9b80dc904da5b0bbf342
m0m0sha <vova.bratsun@gmail.com>
2024-05-24 19:15:34 +0300

    Fix database configuration and add dj-database-url

--- VKR/settings.py
+++ VKR/settings.py
@@ -94,15 +94,21 @@ WSGI_APPLICATION = 'VKR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'vkr',  # Имя базы данных
+#        'USER': 'postgres',  # Имя пользователя
+#        'PASSWORD': 'ghbdtnkjk',  # Пароль
+#        'PORT': '5432',  # Порт 5432
+#    },
+#    'default': dj_database_url.config(default='postgres://vova:ghbdtnkjk@147.45.247.107:5432/vkr')
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vkr',  # Имя базы данных
-        'USER': 'postgres',  # Имя пользователя
-        'PASSWORD': 'ghbdtnkjk',  # Пароль
-        'PORT': '5432',  # Порт 5432
-    },
-    'default': dj_database_url.config(default='postgres://user:password@localhost/dbname')
+    'default':
+        dj_database_url.config(default='postgres://vova:ghbdtnkjk@147.45.247.107:5432/vkr')
 }
 
 # Password validation
